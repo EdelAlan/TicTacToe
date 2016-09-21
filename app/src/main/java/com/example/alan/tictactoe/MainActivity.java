391fb7a7263df2163ac74e4288fac887579b1c40
EdelAlan <my.magic@mail.ru>
2016-09-22 01:22:30 +0800

    Overall game is ready to play but only between two people. It has the basic functionality of tic tac toe. in MainActivity.java file: -added turns counter -added endGame() method, which checks the board for winner after each turn. Also, it prints the message if there is a winner or if there is a draw, and the buttons become unclickable.

--- app/src/main/java/com/example/alan/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/alan/tictactoe/MainActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button b1, b2, b3, b4, b5, b6, b7, b8, b9;
 
-    boolean isTurn;
+    boolean isTurn; //true - X's turn, false = O's turn
+    int turnCount = 0; // counts number of turns
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
                         b1.setText("O");
                     }
                 }
+                turnCount++;
+                endGame();
             }
         });
 
@@ -55,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
                         b2.setText("O");
                     }
                 }
+                turnCount++;
+                endGame();
             }
         });
 
@@ -70,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
                         b3.setText("O");
                     }
                 }
+                turnCount++;
+                endGame();
             }
         });
 
@@ -77,14 +85,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (b4.getText().toString().equals("")) {
-                                    if (isTurn) {
-                                        isTurn = false;
-                                        b4.setText("X");
-                                    } else if (!isTurn) {
-                                        isTurn = true;
-                                        b4.setText("O");
-                                    }
-                                }
+                    if (isTurn) {
+                        isTurn = false;
+                        b4.setText("X");
+                    } else if (!isTurn) {
+                        isTurn = true;
+                        b4.setText("O");
+                    }
+                }
+                turnCount++;
+                endGame();
             }
         });
 
@@ -92,14 +102,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (b5.getText().toString().equals("")) {
-                                    if (isTurn) {
-                                        isTurn = false;
-                                        b5.setText("X");
-                                    } else if (!isTurn) {
-                                        isTurn = true;
-                                        b5.setText("O");
-                                    }
-                                }
+                    if (isTurn) {
+                        isTurn = false;
+                        b5.setText("X");
+                    } else if (!isTurn) {
+                        isTurn = true;
+                        b5.setText("O");
+                    }
+                }
+                turnCount++;
+                endGame();
             }
         });
 
@@ -107,14 +119,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (b6.getText().toString().equals("")) {
-                                    if (isTurn) {
-                                        isTurn = false;
-                                        b6.setText("X");
-                                    } else if (!isTurn) {
-                                        isTurn = true;
-                                        b6.setText("O");
-                                    }
-                                }
+                    if (isTurn) {
+                        isTurn = false;
+                        b6.setText("X");
+                    } else if (!isTurn) {
+                        isTurn = true;
+                        b6.setText("O");
+                    }
+                }
+                turnCount++;
+                endGame();
             }
         });
 
@@ -122,14 +136,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (b7.getText().toString().equals("")) {
-                                    if (isTurn) {
-                                        isTurn = false;
-                                        b7.setText("X");
-                                    } else if (!isTurn) {
-                                        isTurn = true;
-                                        b7.setText("O");
-                                    }
-                                }
+                    if (isTurn) {
+                        isTurn = false;
+                        b7.setText("X");
+                    } else if (!isTurn) {
+                        isTurn = true;
+                        b7.setText("O");
+                    }
+                }
+                turnCount++;
+                endGame();
             }
         });
 
@@ -137,14 +153,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (b8.getText().toString().equals("")) {
-                                    if (isTurn) {
-                                        isTurn = false;
-                                        b8.setText("X");
-                                    } else if (!isTurn) {
-                                        isTurn = true;
-                                        b8.setText("O");
-                                    }
-                                }
+                    if (isTurn) {
+                        isTurn = false;
+                        b8.setText("X");
+                    } else if (!isTurn) {
+                        isTurn = true;
+                        b8.setText("O");
+                    }
+                }
+                turnCount++;
+                endGame();
             }
         });
 
@@ -152,15 +170,72 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (b9.getText().toString().equals("")) {
-                                    if (isTurn) {
-                                        isTurn = false;
-                                        b9.setText("X");
-                                    } else if (!isTurn) {
-                                        isTurn = true;
-                                        b9.setText("O");
-                                    }
-                                }
+                    if (isTurn) {
+                        isTurn = false;
+                        b9.setText("X");
+                    } else if (!isTurn) {
+                        isTurn = true;
+                        b9.setText("O");
+                    }
+                }
+                turnCount++;
+                endGame();
             }
         });
+    }
+
+    private void endGame() {
+        String button1, button2, button3, button4, button5, button6, button7, button8, button9;
+        boolean isOver = false;
+
+        button1 = b1.getText().toString();
+        button2 = b2.getText().toString();
+        button3 = b3.getText().toString();
+        button4 = b4.getText().toString();
+        button5 = b5.getText().toString();
+        button6 = b6.getText().toString();
+        button7 = b7.getText().toString();
+        button8 = b8.getText().toString();
+        button9 = b9.getText().toString();
+
+        // all possible outcomes in the game
+        if (button1.equals("X") && button2.equals("X") && button3.equals("X")
+                || button4.equals("X") && button5.equals("X") && button6.equals("X")
+                || button7.equals("X") && button8.equals("X") && button9.equals("X")
+                || button1.equals("X") && button4.equals("X") && button7.equals("X")
+                || button2.equals("X") && button5.equals("X") && button8.equals("X")
+                || button3.equals("X") && button6.equals("X") && button9.equals("X")
+                || button1.equals("X") && button5.equals("X") && button9.equals("X")
+                || button3.equals("X") && button5.equals("X") && button7.equals("X")) {
+            Toast.makeText(MainActivity.this, "X wins", Toast.LENGTH_LONG).show();
+            isOver = true;
+
+        } else if (button1.equals("O") && button2.equals("O") && button3.equals("O")
+                || button4.equals("O") && button5.equals("O") && button6.equals("O")
+                || button7.equals("O") && button8.equals("O") && button9.equals("O")
+                || button1.equals("O") && button4.equals("O") && button7.equals("O")
+                || button2.equals("O") && button5.equals("O") && button8.equals("O")
+                || button3.equals("O") && button6.equals("O") && button9.equals("O")
+                || button1.equals("O") && button5.equals("O") && button9.equals("O")
+                || button3.equals("O") && button5.equals("O") && button7.equals("O")) {
+            Toast.makeText(MainActivity.this, "O wins", Toast.LENGTH_LONG).show();
+            isOver = true;
+        } else if (turnCount == 9) {
+            Toast.makeText(MainActivity.this, "Draw", Toast.LENGTH_LONG).show();
+            isOver = true;
+        }
+
+        if (isOver) {
+            b1.setEnabled(false);
+            b2.setEnabled(false);
+            b3.setEnabled(false);
+            b4.setEnabled(false);
+            b5.setEnabled(false);
+            b6.setEnabled(false);
+            b7.setEnabled(false);
+            b8.setEnabled(false);
+            b9.setEnabled(false);
+        }
+
     }
 }
